4cca5b727712a1872f89fb2c84fdce597868a1ea
Виталий Тимофеев <vital2505@mail.ru>
2023-06-25 16:22:33 +1000

    code according to PEP8

--- main.py
+++ main.py
@@ -3,29 +3,25 @@ from random import randint
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
+        return (f'{char_name} нанёс урона{5 + randint(3, 5)})
     if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}')
+        return (f'{char_name} 5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
-def defence(char_name, char_class):
+        return (f'{char_name} нанёс урон{5 + randint(-3, -1)')
+    def defence(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-def special(char_name, char_class):
-    if char_class == 'warrior': 
-        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
+    def special(char_name, char_class):
+        if char_class == 'warrior':
+    return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
-
-
-
-
 def start_training(char_name, char_class):
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
@@ -72,6 +68,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-    
-
-main()
